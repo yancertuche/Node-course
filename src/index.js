8d758b0721b57ae7d0e508d7a5cd6b6b3cf10b4a
Yan Carlos Certuche Grueso <yancertuche@gmail.com>
2020-04-26 19:39:36 -0500

    fix erros of write function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-var http= require("http");
+var http = require("http");
 
 var server = http.createServer(
-    function(request, response){
-        if(request.url == "/"){
+    function(request, response) {
+        if(request.url === "/"){
             response.writeHead(200, {"Content-Type": "text/html"});
-            response.write(<html><body><p>Ella no me ama</p></body></html>);
+            response.write("<html><body><p>Ella no te ama</p></body></html>");
             response.end();
-        } else if(request.url == "/Seguro"){
+        } else if(request.url === "/seguro") {
             response.writeHead(200, {"Content-Type": "text/html"});
-            response.write(<html><body><p>Asies amiguito</p></body></html>);
+            response.write("<html><body><p>Asies amiguito</p></body></html>");
             response.end();
-        } else (request.url == "/aQuienAma"){
+        }else {
             response.writeHead(404, {"Content-Type": "text/html"});
-            response.write(<html><body><p>NOT FOUND</p></body></html>);
+            response.write("<html><body><p>NOT FOUND</p></body></html>");
             response.end();
         } 
     }
 
 );
 
-server.liste(4000);
+server.listen(4000);
 console.log("running on 4000");
