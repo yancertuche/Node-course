016048c73bc157adf1ebfac3e8ce8bf5b67c0587
Yan Carlos Certuche Grueso <yancertuche@gmail.com>
2020-04-26 20:12:28 -0500

    ending of practice of importations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+//importaciones de módulos de tipo core, esto es más de lado del servidor
 var http = require("http");
+
+//importaciones de módulos locales, que son lo que se aporvechan para las funciones que se desarrolan. 
+var log = require('./modules/my-log');
+var consts = require('./utils/consts')
+var firebase = require('../libs/firebase')
 
 var server = http.createServer(
     function(request, response) {
@@ -9,6 +15,16 @@ var server = http.createServer(
         } else if(request.url === "/seguro") {
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write("<html><body><p>Asies amiguito</p></body></html>");
+            response.end();
+        }else if(request.url === "/info") {
+            var result= log.info(request.url)
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(result);
+            response.end();
+        }else if(request.url === "/error") {
+            var result= log.error(request.url)
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(result);
             response.end();
         }else {
             response.writeHead(404, {"Content-Type": "text/html"});
