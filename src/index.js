294e455903346922dba01a7da3ac5870120e239d
Yan Carlos Certuche Grueso <yancertuche@gmail.com>
2020-05-03 21:40:13 -0500

    doing query parametric using url

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 //importaciones de módulos de tipo core, esto es más de lado del servidor
 var http = require("http");
+var url  = require('url');
+var querystring = require('querystring')
 
 //importaciones de módulos locales, que son lo que se aporvechan para las funciones que se desarrolan. 
 var {error, info  }= require('./modules/my-log');
@@ -9,19 +11,25 @@ var pais  = require('countries-list')
 
 var server = http.createServer(
     function(request, response) {
-        if(request.url === "/"){
+        var parsed = url.parse(request.url); // para dividir la url y recibir parametros
+        console.log("parsed",parsed)
+        var pathname= parsed.pathname;
+        var query = querystring.parse(parsed.query)
+        console.log("query",query)
+
+        if(pathname === "/"){
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write("<html><body><p>Ella no te ama</p></body></html>");
             response.end();
-        } else if(request.url === "/seguro") {
+        } else if(pathname === "/seguro") {
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write("<html><body><p>Asies amiguito</p></body></html>");
             response.end();
-        }else if(request.url === "/country") {
+        }else if(pathname === "/country") {
             response.writeHead(200, {"Content-Type": "application/json"});
-            response.write(JSON.stringify(pais.countries.CO));
+            response.write(JSON.stringify(pais.countries[query.code] ));
             response.end();
-        }else if(request.url === "/error") {
+        }else if(pathname === "/error") {
             var result= error(request.url)
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write(result);
